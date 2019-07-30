13ade8c837b541ffa380a14b4ed227d49ae5d43d
theron.joe <theronj@joetwebdev.io>
2019-07-30 00:25:13 -0600

    Updates

---
+++ precache-manifest.2ae1057916f4c6a13c5caf549daed9a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5eadfce204b962c23cc0defc1604a0e1",
+    "url": "/my-react-app/index.html"
+  },
+  {
+    "revision": "dd084a65b56dfc37bed3",
+    "url": "/my-react-app/static/css/main.8ef63b32.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/my-react-app/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "dd084a65b56dfc37bed3",
+    "url": "/my-react-app/static/js/main.9d1d2f34.chunk.js"
+  },
+  {
+    "revision": "b28cb091c153ab1cf392",
+    "url": "/my-react-app/static/js/runtime~main.f5ab8794.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
